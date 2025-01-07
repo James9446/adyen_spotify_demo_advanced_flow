4dce2a20a0f2c54f86323a7716d8e6610a2f0e64
James Reynolds <jameshreynolds@gmail.com>
2025-01-07 07:36:53 +0000

    initial advanced flow implementation. The transactions are successfully returning an 'authorised' resultCode but the result of the makeDetailsCall does not contain the actions object

--- public/checkout.js
+++ public/checkout.js
@@ -1,110 +1,213 @@
 // Used to finalize a checkout call in case of redirect
 const urlParams = new URLSearchParams(window.location.search);
-const sessionId = urlParams.get('sessionId');
-const redirectResult = urlParams.get('redirectResult');
-
+const sessionId = urlParams.get("sessionId");
+const redirectResult = urlParams.get("redirectResult");
 
 // Trigger the checkout process on page load
-document.addEventListener('DOMContentLoaded', async () => {
-  try {
-    if (!sessionId) {
-        // new session: start checkout
-        startCheckout();
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        if (!sessionId) {
+            // new session: start checkout
+            startCheckout();
+        } else {
+            // existing session: complete Checkout
+            handleRedirect();
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        alert("Failed to initialize checkout. See console for details.");
     }
-    else {
-        // existing session: complete Checkout
-        handleRedirect()
-    }
-  } catch (error) {
-      console.error('Error:', error);
-      alert('Failed to initialize checkout. See console for details.');
-  }
 });
 
 // Start the checkout process
 async function startCheckout() {
     try {
-        const checkoutDetials = {
+        const checkoutDetails = {
             amount: {
                 value: 10000,
-                currency: "EUR"
+                currency: "EUR",
             },
             countryCode: "NL",
+            locale: "nl-NL",
+            channel: "Web",
             lineItems: [
-                {quantity: 1, amountIncludingTax: 10000 , description: "Premium Membership"},
-            ]
-        }
+                {
+                    quantity: 1,
+                    amountIncludingTax: 10000,
+                    description: "Premium Membership",
+                },
+            ],
+        };
         // Call the server to create a payment session
-        const session = await callServer(`/api/sessions`, checkoutDetials);
+        const paymentMethods = await callServer(
+            `/api/paymentMethods`,
+            checkoutDetails,
+        );
 
         // Create checkout instance using the session returned by the server
-        const checkout = await createCheckoutInstance(session);
+        const checkout = await createCheckoutInstance({
+            paymentMethods,
+            checkoutDetails,
+        });
 
         // Create Drop-in component and mount it
-        checkout.create("dropin", {instantPaymentTypes: ['googlepay']}).mount(document.getElementById('dropin-container'));
+        checkout
+            .create("dropin", { instantPaymentTypes: ["googlepay"] })
+            .mount(document.getElementById("dropin-container"));
     } catch (error) {
-        console.error('Error in startCheckout:', error);
-        alert("Error occurred. Look at console for details");
+        console.error("Error in paymentMethods:", error);
+        // alert("Error occurred. Look at console for details");
     }
 }
 
 // Create and configure checkout instance
-async function createCheckoutInstance(session) {
+async function createCheckoutInstance({ paymentMethods, checkoutDetails }) {
     const clientKey = await getClientKey();
-    const amount = session.amount;
+    const amount = checkoutDetails.amount;
+    const locale = checkoutDetails.locale;
+    const countryCode = checkoutDetails.countryCode;
 
-    // Configure checkout instance 
+    console.log("amount createCheckoutInstance", amount);
+
     const configuration = {
         clientKey,
-        locale: "en_US",
-        environment: "test",  // change to live for production
-        showPayButton: true,
-        session,
-        analytics: {
-            enabled: true
-        },
-        paymentMethodsConfiguration: {
-            card: {
-                hasHolderName: true,
-                holderNameRequired: true,
-                hideCVC: false,
-                name: "Credit or debit card",
-                amount,
-                styles: {
-                    base: {
-                        color: '#FFFFFF',
-                        fontSize: '14px',
-                        lineHeight: '14px',
-                    },
-                    error: {
-                        color: '#FFFFFF'
-                    }
+        environment: "test",
+        amount,
+        locale,
+        countryCode,
+
+        // The full /paymentMethods response object from your server. Contains the payment methods configured in your account.
+        paymentMethodsResponse: paymentMethods,
+
+        onSubmit: async (state, component, actions) => {
+            try {
+                console.log("state: ", state);
+                console.log("component: ", component);
+                console.log("actions: ", actions);
+
+                // Make a POST /payments request from your server.
+                const paymentData = state.data;
+                const reference = crypto.randomUUID();
+                const result = await makePaymentsCall({
+                    paymentData,
+                    countryCode,
+                    locale,
+                    amount,
+                    reference,
+                });
+
+                console.log("result", result);
+                // If the payment is successful, redirect to the success page
+                // If the /payments request from your server fails, or if an unexpected error occurs.
+                if (!result.resultCode) {
+                    actions.reject();
+                    return;
                 }
-            },
-            style: {
-                theme: 'dark',
-                backdrop: 'rgba(0, 0, 0, 0.85)',
+
+                const { resultCode, action, order, donationToken } = result;
+
+                // If the /payments request request form your server is successful, you must call this to resolve whichever of the listed objects are available.
+                // You must call this, even if the result of the payment is unsuccessful.
+                console.log("actions: ", actions);
+                actions.resolve({
+                    resultCode,
+                    action,
+                    order,
+                    donationToken,
+                });
+
+                // if (action) {
+                //   component.handleAction(action);
+                // } else {
+                //   component.setStatus(resultCode);
+                // }
+            } catch (error) {
+                console.error("onSubmit", error);
+                actions.reject();
+            }
+        },
+
+        onAdditionalDetails: async (state, component, actions) => {
+            // try {
+            //     const result = await makeDetailsCall(state.data);
+
+            //     if (!result.resultCode) {
+            //       component.setStatus('error');
+            //       return;
+            //     }
+
+            //     const { resultCode, action } = result;
+
+            //     if (action) {
+            //       component.handleAction(action);
+            //     } else {
+            //       component.setStatus(resultCode);
+            //     }
+            //   } catch (error) {
+            //     console.error("onAdditionalDetails", error);
+            //     component.setStatus('error');
+            //   }
+
+            try {
+                // Make a POST /payments/details request from your server.
+                const result = await makeDetailsCall(state.data);
+
+                // If the /payments/details request from your server fails, or if an unexpected error occurs.
+                if (!result.resultCode) {
+                    actions.reject();
+                    return;
+                }
+
+                const { resultCode, action, order, donationToken } = result;
+
+                // If the /payments/details request request from your server is successful, you must call this to resolve whichever of the listed objects are available.
+                // You must call this, even if the result of the payment is unsuccessful.
+                actions.resolve({
+                    resultCode,
+                    action,
+                    order,
+                    donationToken,
+                });
+            } catch (error) {
+                console.error("onSubmit", error);
+                actions.reject();
             }
         },
         onPaymentCompleted: (result, component) => {
-            handlePaymentResult(result, component);
+            console.log("Payment completed:");
+            console.info(result, component);
+            handlePaymentResult(result);
+        },
+        onPaymentFailed: (result, component) => {
+            console.info(result, component);
         },
         onError: (error, component) => {
             console.error(error.name, error.message, error.stack, component);
-        }
+        },
     };
-  return new AdyenCheckout(configuration);
+
+    return new AdyenCheckout(configuration);
 }
 
+async function makePaymentsCall(paymentData) {
+    try {
+        console.log("makePaymentsCall Data: ", paymentData);
+        const paymentResult = await callServer(`/api/payments`, paymentData);
+        return paymentResult;
+    } catch (error) {
+        console.error(error);
+        alert("Error occurred. Look at console for details");
+    }
+}
 
 // Handle redirects after card challenges
 async function handleRedirect() {
     try {
         // Create checkout instance using Session extracted from query string parameter
-        const checkout = await createCheckoutInstance({id: sessionId});
-        
-        // Submit the extracted redirectResult - triggers the onPaymentCompleted() handler          
-        checkout.submitDetails({details: {redirectResult}});
+        const checkout = await createCheckoutInstance({ id: sessionId });
+
+        // Submit the extracted redirectResult - triggers the onPaymentCompleted() handler
+        checkout.submitDetails({ details: { redirectResult } });
     } catch (error) {
         console.error(error);
         alert("Error occurred. Look at console for details");
@@ -123,8 +226,12 @@ function handlePaymentResult(response) {
         case "Refused":
             changeCheckoutTitle("Payment Refused");
             setTimeout(() => {
-                addPaymentCompleteMessage("We encountered a problem while processing your payment method.");
-                addPaymentCompleteMessage("Please try again, or choose a different payment method to complete your purchase.");
+                addPaymentCompleteMessage(
+                    "We encountered a problem while processing your payment method.",
+                );
+                addPaymentCompleteMessage(
+                    "Please try again, or choose a different payment method to complete your purchase.",
+                );
                 addButton("/checkout", "Continue");
             }, 1500);
             console.log("response.resultCode: ", response.resultCode);
@@ -150,54 +257,57 @@ async function callServer(url, data) {
             "Content-Type": "application/json",
         },
     });
-  
+
     return await response.json();
 }
 
 // ----- Utility functions ------
 
 async function getClientKey() {
-    const response = await fetch('/api/getClientKey');
+    const response = await fetch("/api/getClientKey");
     const data = await response.json();
+    console.log(data.clientKey);
     return data.clientKey;
 }
 
 function changeCheckoutTitle(newTitle) {
-  const titleElement = document.getElementById('checkout-title');
-  if (titleElement) {
-      titleElement.textContent = newTitle;
-  } else {
-      console.error('Checkout title element not found');
-  }
+    const titleElement = document.getElementById("checkout-title");
+    if (titleElement) {
+        titleElement.textContent = newTitle;
+    } else {
+        console.error("Checkout title element not found");
+    }
 }
 
-function addPaymentCompleteMessage(message="Welcome to Spotify Premium! Enjoy music like never before!") {
-  const container = document.querySelector('.checkout-container');
-  
-  // Add payment complete message after session is complete
-  const paymentCompleteMessage = document.createElement('p');
-  paymentCompleteMessage.textContent = message;
-  paymentCompleteMessage.style.marginTop = '20px';
-  container.appendChild(paymentCompleteMessage);
+function addPaymentCompleteMessage(
+    message = "Welcome to Spotify Premium! Enjoy music like never before!",
+) {
+    const container = document.querySelector(".checkout-container");
+
+    // Add payment complete message after session is complete
+    const paymentCompleteMessage = document.createElement("p");
+    paymentCompleteMessage.textContent = message;
+    paymentCompleteMessage.style.marginTop = "20px";
+    container.appendChild(paymentCompleteMessage);
 }
 
-function addButton(href="/", buttonText="Explore Your Benefits") {
-  const container = document.querySelector('.checkout-container');
-  
-  // Add button to navigate back to homepage
-  const button = document.createElement('button');
-  button.textContent = buttonText;
-  button.style.marginTop = '20px';
-  button.style.padding = '10px 20px';
-  button.style.backgroundColor = '#1DB954';
-  button.style.color = 'white';
-  button.style.border = 'none';
-  button.style.borderRadius = '20px';
-  button.style.cursor = 'pointer';
-  
-  button.addEventListener('click', () => {
-      window.location.href = href; // Adjust this if your homepage URL is different
-  });
-  
-  container.appendChild(button);
+function addButton(href = "/", buttonText = "Explore Your Benefits") {
+    const container = document.querySelector(".checkout-container");
+
+    // Add button to navigate back to homepage
+    const button = document.createElement("button");
+    button.textContent = buttonText;
+    button.style.marginTop = "20px";
+    button.style.padding = "10px 20px";
+    button.style.backgroundColor = "#1DB954";
+    button.style.color = "white";
+    button.style.border = "none";
+    button.style.borderRadius = "20px";
+    button.style.cursor = "pointer";
+
+    button.addEventListener("click", () => {
+        window.location.href = href; // Adjust this if your homepage URL is different
+    });
+
+    container.appendChild(button);
 }
--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const path = require('path');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const { uuid } = require('uuidv4');
-const { hmacValidator } = require('@adyen/api-library');
-const { Client, Config, CheckoutAPI } = require("@adyen/api-library");
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const { uuid } = require("uuidv4");
+const { hmacValidator } = require("@adyen/api-library");
+const { Client, Config, CheckoutAPI, Types } = require("@adyen/api-library");
 
 // Load environment variables
 dotenv.config();
@@ -13,45 +13,94 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 // Adyen NodeJS library configuration
 const config = new Config();
+// console.log("process.env.ADYEN_API_KEY", process.env.ADYEN_API_KEY);
 config.apiKey = process.env.ADYEN_API_KEY;
 const client = new Client({ config });
-client.setEnvironment("TEST");  // change to LIVE for production
+client.setEnvironment("TEST"); // change to LIVE for production
 const checkout = new CheckoutAPI(client);
 
-// API Endpoints
-app.post("/api/sessions", async (req, res) => {
+// ----- API ENDPOINTS -----
+// GET Payment Methods
+app.post("/api/paymentMethods", async (req, res) => {
+  console.log("/paymentMethods start");
   try {
-    const orderRef = uuid();
-    const address = req.get('host'); // testing on localhost:3000
-    const protocol = req.socket.encrypted ? 'https' : 'http';
     const amount = {
       currency: req.body.amount.currency,
-      value: req.body.amount.value
+      value: req.body.amount.value,
     };
     const countryCode = req.body.countryCode;
+    const shopperLocale = req.body.locale;
+    const channel = req.body.channel;
     const lineItems = req.body.lineItems;
 
-    const response = await checkout.PaymentsApi.sessions({
+    const paymentMethodRequest = {
       amount,
       countryCode,
       merchantAccount: process.env.ADYEN_MERCHANT_ACCOUNT,
-      reference: orderRef,
-      returnUrl: `${protocol}://${address}/checkout?orderRef=${orderRef}`,
+      shopperLocale,
+      channel,
       lineItems,
-    });
+    };
+    // console.log("request body:", paymentMethodRequest);
+    const response = await checkout.PaymentsApi.paymentMethods(
+      paymentMethodRequest,
+      {
+        idempotencyKey: uuid(),
+      },
+    );
 
-    // return the session to the client i.e. return the CreateCheckoutSessionResponse object
+    // return the payment methods to the client i.e. return the CreateCheckoutSessionResponse object
     res.json(response);
   } catch (err) {
-    console.error(`Error: ${err.message}, error code: ${err.errorCode}, stack: ${err.stack}`);
-    res.status(err.statusCode || 500).json({ error: 'An error occurred during payment processing' });
+    console.error(
+      `Error: ${err.message}, error code: ${err.errorCode}, stack: ${err.stack}`,
+    );
+    res
+      .status(err.statusCode || 500)
+      .json({ error: "An error occurred during payment processing" });
+  }
+});
+
+// Make a Payment
+app.post("/api/payments", async (req, res) => {
+  console.log("/payment");
+  try {
+    // console.log("payment request body", req.body);
+    const amount = req.body.amount;
+    const reference = req.body.reference;
+    const paymentMethod = req.body.paymentData.paymentMethod;
+    const riskData = req.body.paymentData.riskData;
+
+    // Create the request object(s)
+    const paymentRequest = {
+      amount,
+      reference,
+      paymentMethod,
+      riskData,
+      returnUrl: "https://207a399b-3262-4a38-908f-787ffe2ae23d-00-1ry6k6zlua0j2.picard.replit.dev/checkout",
+      merchantAccount: process.env.ADYEN_MERCHANT_ACCOUNT,
+    };
+
+    // console.log('request body:', paymentRequest);
+    const response = await checkout.PaymentsApi.payments(paymentRequest, {
+      idempotencyKey: uuid(),
+    });
+
+    res.json(response);
+  } catch (err) {
+    console.error(
+      `Error: ${err.message}, error code: ${err.errorCode}, stack: ${err.stack}`,
+    );
+    res
+      .status(err.statusCode || 500)
+      .json({ error: "An error occurred during payment processing" });
   }
 });
 
@@ -60,41 +109,44 @@ app.post("/api/webhooks/notifications", async (req, res) => {
   const hmacKey = process.env.ADYEN_HMAC_KEY;
   const validator = new hmacValidator();
   const notificationRequest = req.body;
-  const notificationRequestItems = notificationRequest.notificationItems
-  const notification = notificationRequestItems[0].NotificationRequestItem
-  
+  const notificationRequestItems = notificationRequest.notificationItems;
+  const notification = notificationRequestItems[0].NotificationRequestItem;
+
   if (validator.validateHMAC(notification, hmacKey)) {
     const merchantReference = notification.merchantReference;
     const eventCode = notification.eventCode;
-    console.log("merchantReference:" + merchantReference + " eventCode:" + eventCode);
-    
+    console.log(
+      "merchantReference:" + merchantReference + " eventCode:" + eventCode,
+    );
+
     // Consume event asynchronously
     consumeEvent(notification);
-    
+
     res.status(202).send();
   } else {
     console.log("Invalid HMAC signature: " + notification);
-    res.status(401).send('Invalid HMAC signature');
+    res.status(401).send("Invalid HMAC signature");
   }
 });
 
 // possible todo: implement event consumption logic
 function consumeEvent(notification) {
   // Implement event consumption logic here
+  console.log("conusmeEvent", notification);
 }
 
 // Serve the index.html file for the root route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
-// Server the checkout.html file 
-app.get('/checkout', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'checkout.html'));
+// Server the checkout.html file
+app.get("/checkout", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "checkout.html"));
 });
 
 // Serve the clientKey to the client
-app.get('/api/getClientKey', (req, res) => {
+app.get("/api/getClientKey", (req, res) => {
   res.json({ clientKey: process.env.ADYEN_CLIENT_KEY });
 });
 
